514531611bca902b56ec946f401b07a60a01c1f2
Nyxora <nyxora1@gmail.com>
2025-05-24 22:09:04 +0700

    update UI Information

---
+++ Assets/food_information_menu.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FoodSelectionUI : MonoBehaviour
+{
+    [Header("Navigation")]
+    [SerializeField] private GameObject backButtonTarget;
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button menuButton;
+    [SerializeField] private Button startGameButton;
+
+    [Header("Food Display")]
+    [SerializeField] private TMP_Text foodTitleText;
+    [SerializeField] private Image foodImage;
+    [SerializeField] private TMP_Text foodInfoText;
+
+    [Header("Menu Dropdown")]
+    [SerializeField] private GameObject dropdownPanel;
+    [SerializeField] private List<Button> foodButtons; // Assign all your existing food buttons here
+
+    [Header("Food Data")]
+    [SerializeField] private List<FoodData> foodItems = new List<FoodData>();
+
+    private void Awake()
+    {
+        // Initialize navigation buttons
+        backButton.onClick.AddListener(OnBackButtonPressed);
+        menuButton.onClick.AddListener(ToggleDropdown);
+        startGameButton.onClick.AddListener(OnStartGamePressed);
+
+        // Initialize food buttons
+        SetupFoodButtons();
+
+        // Close dropdown initially
+        dropdownPanel.SetActive(false);
+
+        // Select first food by default if there are any
+        if (foodItems.Count > 0 && foodButtons.Count > 0)
+        {
+            SelectFood(foodItems[0]);
+        }
+    }
+
+    private void SetupFoodButtons()
+    {
+        // Make sure we have enough buttons for all food items
+        if (foodButtons.Count < foodItems.Count)
+        {
+            Debug.LogWarning("Not enough buttons for all food items!");
+        }
+
+        // Setup each button with corresponding food data
+        for (int i = 0; i < Mathf.Min(foodButtons.Count, foodItems.Count); i++)
+        {
+            int index = i; // Important for closure
+            foodButtons[i].onClick.AddListener(() => SelectFood(foodItems[index]));
+
+            // You can also set button text or icon here if needed
+            if (foodButtons[i].GetComponentInChildren<TMP_Text>() != null)
+            {
+                foodButtons[i].GetComponentInChildren<TMP_Text>().text = foodItems[i].foodName;
+            }
+        }
+    }
+
+    public void SelectFood(FoodData food)
+    {
+        // Update UI with selected food data
+        foodTitleText.text = food.foodName;
+        foodImage.sprite = food.foodSprite;
+        foodInfoText.text = food.foodDescription;
+
+        // Close dropdown
+        dropdownPanel.SetActive(false);
+    }
+
+    private void OnBackButtonPressed()
+    {
+        if (backButtonTarget != null)
+        {
+            backButtonTarget.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void ToggleDropdown()
+    {
+        dropdownPanel.SetActive(!dropdownPanel.activeSelf);
+    }
+
+    private void OnStartGamePressed()
+    {
+        Debug.Log("Starting game with selected food: " + foodTitleText.text);
+        // Add your game start logic here
+    }
+}
+
+[System.Serializable]
+public class FoodData
+{
+    public string foodName;
+    [TextArea] public string foodDescription;
+    public Sprite foodSprite;
+}
--- Assets/main_menu.cs
+++ Assets/main_menu.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class main_menu : MonoBehaviour
 {
+    public GameObject mainmenu, info;
     public void play()
     {
-        SceneManager.LoadScene("level");
+        mainmenu.SetActive(false);
+        info.SetActive(true);
     }
     
     public void quit()
     {
         Application.Quit();
+    }
+
+    public void play_asli()
+    {
+        SceneManager.LoadScene("level");
     }
 }
